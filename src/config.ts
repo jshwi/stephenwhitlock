76c84203a0f14851e807f8867873bd7674364e10
Stephen Whitlock <s.whitlock@live.com>
2025-12-16 08:48:34 +1100

    change: configure website url
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/",
+  website: "https://www.stephenwhitlock.com/",
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
